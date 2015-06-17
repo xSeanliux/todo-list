af285d7b48bf77ccf04f906d193fd133fa0964c1
xSeanliux <zhxnliu@yahoo.com.tw>
2015-06-17 21:35:30 +0800

    Edited code

--- todolist.js
+++ todolist.js
@@ -1,17 +1,35 @@
+
+
+//Making the background color start
 var r = Math.floor( (Math.random()*256) ),
     g = Math.floor( (Math.random()*256) ),
     b = Math.floor( (Math.random()*256) );
-var colorHex = "#"+r.toString(16)+g.toString(16)+b.toString(16);
+var colorHex = "rgb("+r+","+g+","+b+")";
+
 document.body.style.backgroundColor = colorHex;
+
+//background color end. 
+
 
 function getValue(){
     var x=document.getElementById("myHeader");
     var node = document.createElement("LI");
     var n = prompt("Add something~");
     node.className = "list";
+    node.id = "item";
     node.innerHTML = n;
     document.getElementById("myList").appendChild(node);
     var c = document.getElementById("checkbox").cloneNode(true);
-    document.getElementById("hehe").appendChild(c);
+   
+}
+
+
+function deleteObject(){
+
+    $("#myList #item").remove('#item');
+
+      
+
+    
 }
 
